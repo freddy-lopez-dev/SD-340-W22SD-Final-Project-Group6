7dd1b4b9a8c99375d7437ab688b36dfbe86ba489
Fulin <944799613@qq.com>
2022-09-20 01:25:08 -0500

    Upper First Lecter Done

--- Models/Project.cs
+++ Models/Project.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace SD_340_W22SD_Final_Project_Group6.Models
 {
@@ -8,6 +9,11 @@ namespace SD_340_W22SD_Final_Project_Group6.Models
         [StringLength(200, ErrorMessage = "Project Name should be from 5 upto 200 characters only")]
         [MinLength(5)]
         [Required]
+        [DisplayName("Project Name :")]
+        [RegularExpression(@"[A-Z][A-Za-z0-9]{5,200}$",
+         ErrorMessage = "Only uppercase Characters are allowed!")]
+
+
         public string ProjectName { get; set; }
         public ApplicationUser? CreatedBy { get; set; }
         public ICollection<UserProject>? AssignedTo { get; set; } = new HashSet<UserProject>();
@@ -15,3 +21,7 @@ namespace SD_340_W22SD_Final_Project_Group6.Models
 
     }
 }
+
+
+
+
