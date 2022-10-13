290ef60a15bf7dbefde7226e3a33492ea5d18ed2
Fethi <fethiarjouni@student.mitt.ca>
2022-10-13 18:13:46 -0500

    Update the ProjectBusinessLogic

--- BLL/ProjectBusinessLogic.cs
+++ BLL/ProjectBusinessLogic.cs
@@ -11,44 +11,67 @@ namespace SD_340_W22SD_Final_Project_Group6.BLL
             repo = repository;
         }
 
-        public void AddProject(Project project)
+        public void CreateProject(Project project)
         {
             repo.Add(project);
+            repo.Save();
         }
 
         public void DelelteProject(Project project)
         {
             repo.Delete(project);
+            repo.Save();
         }
 
         public Project GetProject(int id)
         {
-            return repo.Get(id);
+            return repo.Get(project => project.Id == id);
         }
 
-        public Project GetProject(Func<Project, bool> predicate)
-        {
-            return repo.Get(predicate);
-        }
+        //public Project GetProject(Func<Project, bool> predicate)
+        //{
+        //    return repo.Get(predicate);
+        //}
 
-        public List<Project> GetProjects()
+        public List<Project> GetAllProjects()
         {
             return (List<Project>)repo.GetAll();
         }
 
-        public List<Project> GetProjects(Func<Project, bool> predicate)
-        {
-            return (List<Project>)repo.GetList(predicate);
-        }
+        //public List<Project> GetProjects(Func<Project, bool> predicate)
+        //{
+        //    return (List<Project>)repo.GetList(predicate);
+        //}
 
-        public Project UpdateProject(Project project)
+        public void EditProject(Project project)
         {
-            return repo.Update(project);
-        }
-
-        public void SaveChanges()
-        {
+            repo.Update(project);
             repo.Save();
+        }
+
+        public List<Project> OrderedByPriority()
+        {
+            return (List<Project>)repo.GetAll().OrderByDescending(p => p.Tickets.OrderByDescending(t => t.TicketPriority));
+        }
+
+        public List<Project> OrderedByPriorityAsc()
+        {
+            return (List<Project>)repo.GetAll().OrderBy(p => p.Tickets.OrderBy(t => t.TicketPriority));
+        }
+
+        public List<Project> OrderedByRequiredHours()
+        {
+            return (List<Project>)repo.GetAll().OrderByDescending(p => p.Tickets.OrderByDescending(t => t.RequiredHours));
+        }
+
+        public List<Project> OrderedByRequiredHoursAsc()
+        {
+            return (List<Project>)repo.GetAll().OrderBy(p => p.Tickets.OrderBy(t => t.RequiredHours));
+        }
+
+        public List<Project> GetCompletedProjects()
+        {
+            return (List<Project>)repo.GetAll();
         }
     }
 }
