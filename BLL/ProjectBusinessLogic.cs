f212c045b661d344b45bfcedce66ce718258282e
LengNan777 <L2452520160@gmail.com>
2022-10-13 15:30:53 -0500

    replace IRepo with ProjectRepo

--- BLL/ProjectBusinessLogic.cs
+++ BLL/ProjectBusinessLogic.cs
@@ -1,54 +1,83 @@
-﻿using SD_340_W22SD_Final_Project_Group6.DAL;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using SD_340_W22SD_Final_Project_Group6.DAL;
+using SD_340_W22SD_Final_Project_Group6.Data;
 using SD_340_W22SD_Final_Project_Group6.Models;
+using System.Web.Mvc;
 
 namespace SD_340_W22SD_Final_Project_Group6.BLL
 {
     public class ProjectBusinessLogic
     {
-        private IRepository<Project> repo;
-        public ProjectBusinessLogic(IRepository<Project> repository)
+        private readonly ProjectRepository _projectRepository;
+        private readonly TicketRepository _ticketRepository;
+        private readonly UserManager<ApplicationUser> _users;
+
+        public ProjectBusinessLogic(ProjectRepository projectRepository, TicketRepository ticketRepository, UserManager<ApplicationUser> users)
         {
-            repo = repository;
+            _projectRepository = projectRepository;
+            _ticketRepository = ticketRepository;
+            _users = users;
         }
 
         public void AddProject(Project project)
         {
-            repo.Add(project);
+            _projectRepository.Add(project);
         }
 
         public void DelelteProject(Project project)
         {
-            repo.Delete(project);
+            _projectRepository.Delete(project);
         }
 
         public Project GetProject(int id)
         {
-            return repo.Get(id);
+            return _projectRepository.Get(id);
         }
 
         public Project GetProject(Func<Project, bool> predicate)
         {
-            return repo.Get(predicate);
+            return _projectRepository.Get(predicate);
         }
 
         public List<Project> GetProjects()
         {
-            return (List<Project>)repo.GetAll();
+            return (List<Project>)_projectRepository.GetAll();
         }
 
         public List<Project> GetProjects(Func<Project, bool> predicate)
         {
-            return (List<Project>)repo.GetList(predicate);
+            return (List<Project>)_projectRepository.GetList(predicate);
         }
 
         public Project UpdateProject(Project project)
         {
-            return repo.Update(project);
+            return _projectRepository.Update(project);
         }
 
         public void SaveChanges()
         {
-            repo.Save();
+            _projectRepository.Save();
+        }
+
+        public ProjectRepository Get_projectRepository()
+        {
+            return _projectRepository;
+        }
+
+        public void Delete(int? id)
+        {
+            if (id == null || _projectRepository == null)
+            {
+                throw new ArgumentNullException("id");
+            }
+
+            var project = _projectRepository
+                .Get(m => m.Id == id);
+            if (project == null)
+            {
+                throw new ArgumentNullException("project");
+            }
         }
     }
 }
