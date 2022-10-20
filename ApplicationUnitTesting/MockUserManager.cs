b184dcedf40bf066b4c7addc6c3281f4273f4e78
Fethi <fethiarjouni@student.mitt.ca>
2022-10-20 09:42:17 -0500

    Clean MockUserManager

--- ApplicationUnitTesting/MockUserManager.cs
+++ ApplicationUnitTesting/MockUserManager.cs
@@ -20,21 +20,5 @@ namespace ApplicationUnitTests
                 new Mock<ILogger<UserManager<ApplicationUser>>>().Object)
         {
         }
-
-        public static UserManager<ApplicationUser> GetMockUserManager(List<ApplicationUser> users, List<string>? roles)
-        {
-            var mockUserManager = new Mock<MockUserManager>();
-
-            mockUserManager.Setup(x => x.Users).Returns(users.AsQueryable());
-            mockUserManager.Setup(x => x.CreateAsync(It.IsAny<ApplicationUser>(), It.IsAny<string>())).ReturnsAsync(IdentityResult.Success);
-            mockUserManager.Setup(x => x.FindByIdAsync(It.IsAny<string>())).ReturnsAsync((string id) => users.Find(u => u.Id == id));
-            mockUserManager.Setup(x => x.FindByNameAsync(It.IsAny<string>())).ReturnsAsync((string name) => users.Find(u => u.UserName == name));
-            mockUserManager.Setup(x => x.UpdateAsync(It.IsAny<ApplicationUser>())).ReturnsAsync(IdentityResult.Success);
-            mockUserManager.Setup(um => um.GetRolesAsync(It.IsAny<ApplicationUser>())).ReturnsAsync(new List<string> { "Admin", "ProjectManager", "Developer" });
-            mockUserManager.Setup(um => um.AddToRoleAsync(It.IsAny<ApplicationUser>(), It.IsAny<string>())).ReturnsAsync(IdentityResult.Success);
-            mockUserManager.Setup(um => um.GetUsersInRoleAsync(It.IsAny<string>())).ReturnsAsync(new List<ApplicationUser> { new ApplicationUser { UserName = "Developer1" } });
-
-            return mockUserManager.Object;
-        }
     }
 }
